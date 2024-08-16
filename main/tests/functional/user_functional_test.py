fc2fa96a3c3ceb4b59a70192a7c2875b9c3b3048
kinoubenkyou <kinoubenkyou@gmail.com>
2024-08-16 18:17:03 +0700

    Format

--- main/tests/functional/user_functional_test.py
+++ main/tests/functional/user_functional_test.py
@@ -26,10 +26,16 @@ class UserFunctionalTest(StaticLiveServerTestCase):
         password = "dr0wss@p"
 
         self.web_driver.get(f"{self.live_server_url}{reverse("user-create")}")
-        self.web_driver.find_element(By.XPATH, "//input[@name=\"email\"]").send_keys(email)
-        self.web_driver.find_element(By.XPATH, "//input[@name=\"name\"]").send_keys(name)
-        self.web_driver.find_element(By.XPATH, "//input[@name=\"password1\"]").send_keys(password)
-        self.web_driver.find_element(By.XPATH, "//input[@name=\"password2\"]").send_keys(password)
-        self.web_driver.find_element(By.XPATH, "//*[@type=\"submit\"]").click()
+        self.web_driver.find_element(By.XPATH, '//input[@name="email"]').send_keys(
+            email
+        )
+        self.web_driver.find_element(By.XPATH, '//input[@name="name"]').send_keys(name)
+        self.web_driver.find_element(By.XPATH, '//input[@name="password1"]').send_keys(
+            password
+        )
+        self.web_driver.find_element(By.XPATH, '//input[@name="password2"]').send_keys(
+            password
+        )
+        self.web_driver.find_element(By.XPATH, '//*[@type="submit"]').click()
 
         self.assertEqual(self.web_driver.current_url, f"{self.live_server_url}/")
