61227faaf7b09994c4a8b5ccca77a3aa4c013238
kinoubenkyou <kinoubenkyou@gmail.com>
2024-08-18 17:59:20 +0700

    Test

--- main/tests/__init__.py
+++ main/tests/__init__.py
@@ -1,4 +1,5 @@
-__all__ = ["SignInFunctionalTest", "UserCreateFunctionalTest"]
+__all__ = ["SignInFunctionalTest", "UserCreateFunctionalTest", "UserReadFunctionalTest"]
 
 from main.tests.functional.sign_in_functional_test import SignInFunctionalTest
-from main.tests.functional.user_functional_test import UserCreateFunctionalTest
+from main.tests.functional.user_functional_test import UserCreateFunctionalTest, \
+    UserReadFunctionalTest
---
+++ main/tests/functional/login_required_mixin.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.common.by import By
+
+
+class LoginRequiredMixin:
+    fixtures = ["login_required_mixin"]
+
+    def login(self):
+        self.web_driver.find_element(By.XPATH, '//input[@name="username"]').send_keys(
+            "email@email.com"
+        )
+        self.web_driver.find_element(By.XPATH, '//input[@name="password"]').send_keys(
+            "dr0wss@p"
+        )
+        self.web_driver.find_element(By.XPATH, '//*[@type="submit"]').click()
