493d34275a177cd24ceeb22dc53eafea69352fe9
kinoubenkyou <kinoubenkyou@gmail.com>
2024-08-18 17:59:50 +0700

    Refactor

--- main/tests/functional/sign_in_functional_test.py
+++ main/tests/functional/sign_in_functional_test.py
@@ -1,20 +1,11 @@
 from django.urls import reverse
-from selenium.webdriver.common.by import By
 
-from main.tests.functional import FunctionalTestCase
+from main.tests.functional import FunctionalTestCase, LoginRequiredMixin
 
 
-class SignInFunctionalTest(FunctionalTestCase):
-    fixtures = ["sign_in"]
-
+class SignInFunctionalTest(LoginRequiredMixin, FunctionalTestCase):
     def test_success(self):
         self.web_driver.get(f"{self.live_server_url}{reverse("sign-in")}")
-        self.web_driver.find_element(By.XPATH, '//input[@name="username"]').send_keys(
-            "email@email.com"
-        )
-        self.web_driver.find_element(By.XPATH, '//input[@name="password"]').send_keys(
-            "dr0wss@p"
-        )
-        self.web_driver.find_element(By.XPATH, '//*[@type="submit"]').click()
+        self.login()
 
         self.assertEqual(self.web_driver.current_url, f"{self.live_server_url}/user/")
