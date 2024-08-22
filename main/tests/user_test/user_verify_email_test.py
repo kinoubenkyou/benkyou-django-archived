42b99ddab7492931623283ada5f110abc823aae1
kinoubenkyou <kinoubenkyou@gmail.com>
2024-08-22 14:48:52 +0700

    Verify email

--- main/tests/user_test/user_verify_email_test.py
+++ main/tests/user_test/user_verify_email_test.py
@@ -15,29 +15,16 @@ class UserVerifyEmailTest(LoginRequiredMixin, TestCase):
         self.web_driver.get(f"{self.live_server_url}/user/verify_email/?code={code}")
         self.login()
         self.web_driver.find_element(By.XPATH, '//*[@type="submit"]').click()
-        self.assertEqual(
-            len(
-                self.find_displayed_elements(
-                    By.XPATH, '//*[normalize-space(text())="Email verified: True"]'
-                )
-            ),
-            1,
-        )
+
+        self.assertEqual(self.web_driver.current_url, f"{self.live_server_url}/user/")
+        self.assertEqual(len(self.find_elements("Email verified: True")), 1)
 
     def test_expired_email_verification(self):
         self.web_driver.get(f"{self.live_server_url}/user/verify_email/?code=code")
         self.login()
         self.web_driver.find_element(By.XPATH, '//*[@type="submit"]').click()
 
-        self.assertEqual(
-            len(
-                self.find_displayed_elements(
-                    By.XPATH,
-                    '//*[normalize-space(text())="Email verification expired."]',
-                )
-            ),
-            1,
-        )
+        self.assertEqual(len(self.find_elements("Email verification expired.")), 1)
 
     def test_not_matched_code(self):
         cache.set(1, "-code")
@@ -46,12 +33,4 @@ class UserVerifyEmailTest(LoginRequiredMixin, TestCase):
         self.login()
         self.web_driver.find_element(By.XPATH, '//*[@type="submit"]').click()
 
-        self.assertEqual(
-            len(
-                self.find_displayed_elements(
-                    By.XPATH,
-                    '//*[normalize-space(text())="Bad Request (400)"]',
-                )
-            ),
-            1,
-        )
+        self.assertEqual(len(self.find_elements("Bad Request (400)")), 1)
