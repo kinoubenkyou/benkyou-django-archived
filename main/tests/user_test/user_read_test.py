bf58311b23f072248700fb37e46ea0e4649f14e6
kinoubenkyou <kinoubenkyou@gmail.com>
2024-08-22 14:48:58 +0700

    Read user

--- main/tests/user_test/user_read_test.py
+++ main/tests/user_test/user_read_test.py
@@ -1,5 +1,3 @@
-from selenium.webdriver.common.by import By
-
 from main.tests.login_required_mixin import LoginRequiredMixin
 from main.tests.test_case import TestCase
 
@@ -9,7 +7,6 @@ class UserReadTest(LoginRequiredMixin, TestCase):
         self.web_driver.get(f"{self.live_server_url}/user/")
         self.login()
 
-        self.web_driver.find_element(
-            By.XPATH, '//*[contains(text(), "email@email.com")]'
-        )
-        self.web_driver.find_element(By.XPATH, '//*[contains(text(), "name")]')
+        self.assertEqual(len(self.find_elements("Email: email@email.com")), 1)
+        self.assertEqual(len(self.find_elements("Email verified: True")), 1)
+        self.assertEqual(len(self.find_elements("Name: name")), 1)
